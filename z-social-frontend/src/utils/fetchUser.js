d09b024da9f317675e4e5e2290ac9200787ef2fd
Aditya Bansal <adityabansalsafe@gmail.com>
2024-09-18 03:38:39 +0530

    Added Frontend repo

--- z-social-frontend/src/utils/fetchUser.js
+++
@@ -1,4 +0,0 @@
-export const fetchUser = () => {
-    const userInfo = localStorage.getItem('user') !== 'undefine' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
-    return userInfo;
-}
